f5ac512e0b4b441f0fc73f9b2d3dd38996644e71
hhhhhq <Mr.j@tom.com>
2021-02-10 19:20:33 +0800

    before deploy

--- src/components/ProductionDetails/index.js
+++ src/components/ProductionDetails/index.js
@@ -2,10 +2,11 @@
  * @Author: hhhhhq
  * @Date: 2021-02-06 15:35:09
  * @LastEditors: hhhhhq
- * @LastEditTime: 2021-02-10 09:23:05
+ * @LastEditTime: 2021-02-10 11:07:12
  * @Description: file content
  */
 import React from "react"
+import Collapse from "rc-collapse"
 import {
   ProductionDescWrapper,
   ProductionDetailsContainer,
@@ -17,11 +18,14 @@ import {
   ProductionWrapper,
 } from "./ProductionDetailsElements"
 import ProductionArray from "./ProductionsDetails"
+import "rc-collapse/assets/index.css"
 
 const ProductionDetails = ({ productId }) => {
   const currentProduction = ProductionArray.filter(
     production => production.id === productId
   )[0]
+
+  const Panel = Collapse.Panel
   return (
     <>
       <ProductionDetailsContainer>
@@ -35,7 +39,16 @@ const ProductionDetails = ({ productId }) => {
           <ProductionInfoWrapper>
             <ProductionInfo>
               <ProductionH1>{currentProduction.name}</ProductionH1>
-              <ProductionDescWrapper></ProductionDescWrapper>
+              <ProductionDescWrapper>
+                <Collapse accordion={true}>
+                  <Panel header="香型">酱香型</Panel>
+                  <Panel header="配料表">红缨糯高粱、小麦、水</Panel>
+                  <Panel header="净含量">{currentProduction.vol}</Panel>
+                  <Panel header="酿造工艺">固态发酵酿造法</Panel>
+                  <Panel header="窖藏时间">{currentProduction.time}</Panel>
+                  <Panel header="特点">{currentProduction.desc}</Panel>
+                </Collapse>
+              </ProductionDescWrapper>
             </ProductionInfo>
           </ProductionInfoWrapper>
         </ProductionWrapper>
